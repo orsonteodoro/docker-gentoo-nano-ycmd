a4f252befc85ab8cbf5a3d54622c63212a14ab09
Orson Teodoro <orsonteodoro@hotmail.com>
2018-02-12 22:23:44 -0800

    clear out the old tests

--- travisci-test.sh
+++ travisci-test.sh
@@ -22,7 +22,5 @@
 echo "travisci=$1"
 travisci="$1"
 if [ -n "$travisci" ]; then
-	echo "This is a simple tests to see if it is pax marked properly."
-	echo $(spotify --help) | grep 'Output the version of the app.' || return 1
-	paxctl -v /usr/share/spotify/spotify | grep -e " -----m------ " || return 1
+	#todo
 fi
